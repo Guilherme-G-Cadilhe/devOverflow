67f09764dfbf8c877b9f8043c0c26779f3a6e8f6
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2023-11-09 15:07:55 -0300

    fixing public routes for webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -50,6 +50,7 @@ export async function POST(req: Request) {
   }
 
   const eventType = evt.type;
+  console.log("eventType :>> ", eventType);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
